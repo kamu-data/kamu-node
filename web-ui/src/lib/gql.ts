ac0aecdad9b387160b649060aed4747cba5932a6
Sergii Mikhtoniuk <mikhtoniuk@gmail.com>
2021-09-06 19:13:09 -0700

    Externalize web-ui config

--- web-ui/src/lib/gql.ts
+++ web-ui/src/lib/gql.ts
@@ -1,3 +1,4 @@
+import { browser } from '$app/env';
 import { ApolloClient, HttpLink, InMemoryCache, gql } from '@apollo/client/core';
 import type { NormalizedCacheObject } from '@apollo/client';
 export { gql } from '@apollo/client/core';
@@ -5,8 +6,14 @@ export { gql } from '@apollo/client/core';
 var client: ApolloClient<NormalizedCacheObject> = null;
 
 function createClient() {
+    let url = <string>(
+        browser
+            ? import.meta.env.VITE_API_SERVER_URL_BROWSER ?? import.meta.env.VITE_API_SERVER_URL
+            : import.meta.env.VITE_API_SERVER_URL
+    );
+
     const link = new HttpLink({
-        uri: 'http://localhost:8080/graphql'
+        uri: url
         // headers: {
         // 	Authorization: `Bearer ${authToken}`
         // }
