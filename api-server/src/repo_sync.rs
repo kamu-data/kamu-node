f279e11e93f67ff60c653f0b0cc9c1fe15222a48
Sergii Mikhtoniuk <mikhtoniuk@gmail.com>
2021-09-07 10:19:26 -0700

    Sync edge cases

--- api-server/src/repo_sync.rs
+++ api-server/src/repo_sync.rs
@@ -4,7 +4,7 @@ use kamu::{
     domain::{MetadataRepository, SyncError, SyncOptions, SyncResult, SyncService},
     infra::RepositoryFactory,
 };
-use opendatafabric::{DatasetRefBuf, RepositoryID};
+use opendatafabric::{DatasetID, DatasetIDBuf, DatasetRefBuf, RepositoryID};
 
 pub fn repo_sync_loop(catalog: dill::Catalog, repo_id: &RepositoryID) {
     let _panic_guard = KillProcessOnPanic;
@@ -30,18 +30,32 @@ pub fn sync_all_from_repo(
     sync_svc: Arc<dyn SyncService>,
     repo_id: &RepositoryID,
 ) -> Result<(), SyncError> {
-    // TODO: This will hit the search limit
-    let datasets_to_sync: Vec<_> = {
-        let repo = metadata_repo.get_repository(repo_id).unwrap();
-        let repo_client = repo_factory.get_repository_client(&repo).unwrap();
+    let datasets_to_sync =
+        get_all_remote_datasets(metadata_repo.as_ref(), repo_factory.as_ref(), repo_id)?;
 
-        let rc = repo_client.lock().unwrap();
-        let res = rc.search(None)?;
-        res.datasets
-            .into_iter()
-            .map(|r| DatasetRefBuf::new(Some(repo_id), r.username(), r.local_id()))
-            .collect()
-    };
+    let local_datasets_to_delete: Vec<DatasetIDBuf> = metadata_repo
+        .get_all_datasets()
+        .filter(|id| {
+            for rem in &datasets_to_sync {
+                if rem.local_id() == (id as &DatasetID) {
+                    return false;
+                }
+            }
+            true
+        })
+        .collect();
+
+    if !local_datasets_to_delete.is_empty() {
+        tracing::info!(
+            "Deleting datasets that are no longer present in repo: {:?}",
+            local_datasets_to_delete
+        );
+
+        // TODO: Handle dangling dependency issue
+        for id in &local_datasets_to_delete {
+            metadata_repo.delete_dataset(id).unwrap();
+        }
+    }
 
     tracing::debug!("Syncing {} datasets", datasets_to_sync.len());
 
@@ -53,6 +67,7 @@ pub fn sync_all_from_repo(
 
     let mut up_to_date = 0;
     let mut updated = 0;
+    let mut diverged = Vec::new();
 
     for ((id, _), res) in sync_results {
         match res {
@@ -60,6 +75,7 @@ pub fn sync_all_from_repo(
                 SyncResult::UpToDate { .. } => up_to_date += 1,
                 SyncResult::Updated { .. } => updated += 1,
             },
+            Err(SyncError::DatasetsDiverged { .. }) => diverged.push(id),
             Err(e) => {
                 tracing::error!("Failed to sync dataset {}: {}", id, e);
                 return Err(e);
@@ -67,21 +83,68 @@ pub fn sync_all_from_repo(
         }
     }
 
-    if updated > 0 {
+    if !diverged.is_empty() {
+        tracing::info!("Force re-syncing datasets: {:?}", diverged);
+
+        // TODO: Handle dangling dependency issue
+        for id in &diverged {
+            metadata_repo.delete_dataset(id.local_id()).unwrap();
+        }
+
+        let sync_results = sync_svc.sync_from_multi(
+            &mut diverged.iter().map(|r| (r.as_ref(), r.local_id())),
+            SyncOptions {},
+            None,
+        );
+
+        for ((id, _), res) in sync_results {
+            match res {
+                Ok(_) => updated += 1,
+                Err(e) => {
+                    tracing::error!("Failed to re-sync dataset {}: {}", id, e);
+                    return Err(e);
+                }
+            }
+        }
+    }
+
+    if updated > 0 || !diverged.is_empty() {
         tracing::info!(
-            "Sync result up-to-date: {}, updated: {}",
+            "Sync result up-to-date: {}, updated: {}, re-synced: {}",
             up_to_date,
-            updated
+            updated,
+            diverged.len(),
         );
     } else {
         tracing::debug!(
-            "Sync result up-to-date: {}, updated: {}",
+            "Sync result up-to-date: {}, updated: {}, re-synced: {}",
             up_to_date,
-            updated
+            updated,
+            diverged.len(),
         );
     }
 
     Ok(())
+}
+
+fn get_all_remote_datasets(
+    metadata_repo: &dyn MetadataRepository,
+    repo_factory: &RepositoryFactory,
+    repo_id: &RepositoryID,
+) -> Result<Vec<DatasetRefBuf>, SyncError> {
+    let repo = metadata_repo.get_repository(repo_id).unwrap();
+    let repo_client = repo_factory.get_repository_client(&repo).unwrap();
+
+    let rc = repo_client.lock().unwrap();
+
+    // TODO: This will hit the search limit
+    let res = rc.search(None)?;
+
+    Ok(res
+        .datasets
+        .into_iter()
+        .map(|r| DatasetRefBuf::new(Some(repo_id), r.username(), r.local_id()))
+        .collect())
 }
 
 struct KillProcessOnPanic;
