4a01f6b42d165c4e57beb0dce7cc232beaf41a4f
Yuliya Kulikovskaya <kulykivska.yuliya@gmail.com>
2021-09-30 18:22:32 +0300

    Adaptivity for AppSearchHeaderComponent. Close menu after entering of search value

--- web-prototype/src/app/app-header/app-header.component.ts
+++ web-prototype/src/app/app-header/app-header.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, OnInit, Output} from "@angular/core";
+import {Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild} from "@angular/core";
 
 @Component({
   selector: 'app-header',
@@ -8,14 +8,20 @@ export class AppHeaderComponent {
     @Input() public searchValue: string;
     @Input() public appLogo: string;
     @Input() public isMobileView: boolean;
+
     @Output() public onInputSearch: EventEmitter<string> = new EventEmitter();
     @Output() public addNew: EventEmitter<null> = new EventEmitter();
     @Output() public userInfo: EventEmitter<null> = new EventEmitter();
     @Output() public onOpenMenu: EventEmitter<null> = new EventEmitter();
 
+    @ViewChild('appHeaderMenuButton') appHeaderMenuButton: ElementRef<HTMLElement>;
+
 
     public onSearch(value: string): void {
         this.onInputSearch.emit(value);
+        setTimeout(() => {
+          this.triggerMenuClick();
+        }, 200)
     }
     public onAddNew(): void {
         this.addNew.emit();
@@ -25,5 +31,12 @@ export class AppHeaderComponent {
     }
     public onToggleMenu(): void {
         this.onOpenMenu.emit();
+    }
+
+    public triggerMenuClick(): void {
+        const el: HTMLElement = this.appHeaderMenuButton.nativeElement;
+        el.focus();
+        el.click();
+        el.blur();
     }
 }
