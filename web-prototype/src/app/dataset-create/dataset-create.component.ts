1ab4d733908b662ea065278e2e2ba5e52da1ad4e
Yuliya Kulikovskaya <kulykivska.yuliya@gmail.com>
2021-10-14 18:49:56 +0300

    KAMU-31 [Web] Create DataSet Module. SelectDataSetTypeComponent

--- web-prototype/src/app/dataset-create/dataset-create.component.ts
+++ web-prototype/src/app/dataset-create/dataset-create.component.ts
@@ -3,6 +3,7 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app-dataset-create',
   templateUrl: './dataset-create.component.html',
+  styleUrls: ['./dataset-create.component.sass']
 })
 export class DatasetCreateComponent {
 }
