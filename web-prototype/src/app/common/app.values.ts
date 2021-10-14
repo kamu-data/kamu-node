8a354b7ab720590279a1e1771245c1cea00b9101
Yuliya Kulikovskaya <kulykivska.yuliya@gmail.com>
2021-10-14 13:02:36 +0300

    KAMU-46 [Web] Application should have new logo

--- web-prototype/src/app/common/app.values.ts
+++ web-prototype/src/app/common/app.values.ts
@@ -2,8 +2,7 @@ import {Injectable} from "@angular/core";
 
 @Injectable()
 export default class AppValues {
-  public static appTitle: string = 'Kamu Web';
-  public static appLogo: string = 'assets/icons/kamu-logo.svg';
+  public static appLogo: string = 'assets/icons/kamu_logo_icon.svg';
 
   public static capitalizeFirstLetter(text: string): string {
     return text.charAt(0).toUpperCase() + text.slice(1);
