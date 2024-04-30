369d955ac23b1e49c5bc150776573735e206fa38
Dima Pristupa <dima@pristupa.dev>
2024-04-30 16:10:50 +0300

    Config: add "repo_url" into RepoConfig

--- src/app/api-server/src/config.rs
+++ src/app/api-server/src/config.rs
@@ -9,7 +9,8 @@
 
 use std::path::PathBuf;
 
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize, Deserializer, Serialize};
+use url::Url;
 
 /////////////////////////////////////////////////////////////////////////////////////////
 
@@ -81,6 +82,8 @@ pub struct AuthProviderConfigDummy {
 #[serde(deny_unknown_fields)]
 #[serde(rename_all = "camelCase")]
 pub struct RepoConfig {
+    #[serde(deserialize_with = "parse_repo_url_opt", default)]
+    pub repo_url: Option<Url>,
     pub caching: RepoCachingConfig,
 }
 
@@ -94,3 +97,37 @@ pub struct RepoCachingConfig {
     // Stores metadata blocks in a local directory to avoid many tiny S3 requests
     pub metadata_local_fs_cache_path: Option<PathBuf>,
 }
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+fn value_parse_repo_url(s: &str) -> Result<Url, &'static str> {
+    match Url::parse(s) {
+        Ok(url) => Ok(url),
+        Err(_) => match PathBuf::from(s).canonicalize() {
+            Ok(path) => Ok(Url::from_file_path(path).unwrap()),
+            Err(_) => Err(
+                "Invalid repo-url, should be a path or a URL in form: file:///home/me/workspace",
+            ),
+        },
+    }
+}
+
+fn parse_repo_url_opt<'de, D>(deserializer: D) -> Result<Option<Url>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    use serde::de::Error;
+
+    let maybe_value = Option::<String>::deserialize(deserializer)?;
+
+    match maybe_value {
+        Some(value) => {
+            let url = value_parse_repo_url(value.as_str()).map_err(Error::custom)?;
+
+            Ok(Some(url))
+        }
+        None => Ok(None),
+    }
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////
