c871393089b0130155694926a79d669ac667e7b6
Sergei Zaychenko <szaychenko@kamu.dev>
2024-12-19 20:58:43 +0200

    Updated API server to match flow configs branch

--- src/app/api-server/src/database.rs
+++ src/app/api-server/src/database.rs
@@ -55,6 +55,7 @@ pub(crate) fn configure_database_components(
 
             b.add::<kamu_flow_system_postgres::PostgresFlowConfigurationEventStore>();
             b.add::<kamu_flow_system_postgres::PostgresFlowEventStore>();
+            b.add::<kamu_flow_system_postgres::PostgresFlowTriggerEventStore>();
 
             b.add::<kamu_task_system_postgres::PostgresTaskEventStore>();
 
@@ -75,6 +76,7 @@ pub(crate) fn configure_database_components(
 
             b.add::<kamu_flow_system_sqlite::SqliteFlowConfigurationEventStore>();
             b.add::<kamu_flow_system_sqlite::SqliteFlowEventStore>();
+            b.add::<kamu_flow_system_sqlite::SqliteFlowTriggerEventStore>();
 
             b.add::<kamu_task_system_sqlite::SqliteTaskSystemEventStore>();
 
@@ -105,6 +107,7 @@ pub(crate) fn configure_in_memory_components(b: &mut CatalogBuilder) {
     b.add::<kamu_accounts_inmem::InMemoryAccessTokenRepository>();
     b.add::<kamu_flow_system_inmem::InMemoryFlowConfigurationEventStore>();
     b.add::<kamu_flow_system_inmem::InMemoryFlowEventStore>();
+    b.add::<kamu_flow_system_inmem::InMemoryFlowTriggerEventStore>();
     b.add::<kamu_task_system_inmem::InMemoryTaskEventStore>();
     b.add::<kamu_datasets_inmem::InMemoryDatasetEnvVarRepository>();
     b.add::<kamu_datasets_inmem::InMemoryDatasetEntryRepository>();
