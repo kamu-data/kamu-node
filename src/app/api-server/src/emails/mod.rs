665961f99353261e0d69c74745b0bc0429155843
rmn-boiko <roman.bv20@gmail.com>
2025-02-06 20:53:17 +0100

    Add access token email norifier

--- src/app/api-server/src/emails/mod.rs
+++ src/app/api-server/src/emails/mod.rs
@@ -7,8 +7,10 @@
 // the Business Source License, use of this software will be governed
 // by the Apache License, Version 2.0.
 
+mod access_token_notifier;
 mod account_lifecycle_notifier;
 mod flow_progress_notifier;
 
+pub use access_token_notifier::*;
 pub use account_lifecycle_notifier::*;
 pub use flow_progress_notifier::*;
