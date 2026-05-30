b55ebdf50c084860f4df1b423fe42b8843577219
Sergii Mikhtoniuk <mikhtoniuk@gmail.com>
2026-05-29 18:07:14 -0700

    Add nix flake for postgres

---
+++ images/nix-dev/flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Dev environment with supporting tools";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.postgresql
+          ];
+        };
+      });
+}
